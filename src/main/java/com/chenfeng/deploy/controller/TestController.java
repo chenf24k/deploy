6194df55caea34302fc3a1fbdb8c3524e2374ba4
陈枫 <chenfeng@xiaoma.wang>
2019-08-24 17:15:36 +0800

    增加now()方法 参数 pattern显示格式 返回 按传入的格式返回当前时间

--- src/main/java/com/chenfeng/deploy/controller/TestController.java
+++ src/main/java/com/chenfeng/deploy/controller/TestController.java
@@ -5,6 +5,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 @Controller
 public class TestController {
     @RequestMapping("/")
@@ -17,6 +21,17 @@ public class TestController {
     @ResponseBody
     public String hello() {
         return "Hello,world!";
+    }
+
+    @GetMapping("/time")
+    @ResponseBody
+    public String now(String pattern) {
+        if (pattern == null) {
+            DateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss SSS");
+            return df.format(new Date());
+        }
+        DateFormat df = new SimpleDateFormat(pattern);
+        return df.format(new Date());
     }
 }
 
